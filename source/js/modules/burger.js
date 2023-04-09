443f72a9fb5cb7efb40bb0bb03cfa32aa3209612
Artem <kohi881@gmail.com>
2023-04-09 22:21:29 +0300

    burger refactor

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -6,9 +6,11 @@ burgerButton.addEventListener('click', function () {
   if (mainNav.classList.contains('header__nav--opened')) {
     mainNav.classList.remove('header__nav--opened');
     mainNav.classList.add('header__nav--closed');
+    document.body.classList.remove('scroll-lock');
   } else {
     mainNav.classList.add('header__nav--opened');
     mainNav.classList.remove('header__nav--closed');
+    document.body.classList.add('scroll-lock');
   }
 });
 
@@ -21,5 +23,13 @@ burgerLink.forEach((element) => {
   element.addEventListener('click', function () {
     mainNav.classList.remove('header__nav--opened');
     mainNav.classList.add('header__nav--closed');
+    document.body.classList.remove('scroll-lock');
   });
+});
+
+document.addEventListener('click', (e) => {
+  if (!mainNav.contains(e.target)) {
+    mainNav.classList.remove('header__nav--opened');
+    mainNav.classList.add('header__nav--closed');
+  }
 });
