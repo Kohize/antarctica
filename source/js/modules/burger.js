85474613156f8dfb350f87e0d3f10989036a33dc
Artem <kohi881@gmail.com>
2023-04-10 15:49:15 +0300

    burger.js refactor

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -2,17 +2,20 @@ const mainNav = document.querySelector('.header__nav');
 const burgerButton = document.querySelector('.header__burger');
 const burgerLink = document.querySelectorAll('.header__link');
 
-burgerButton.addEventListener('click', function () {
-  if (mainNav.classList.contains('header__nav--opened') && mainNav && burgerButton) {
-    mainNav.classList.remove('header__nav--opened');
-    mainNav.classList.add('header__nav--closed');
-    document.body.classList.remove('scroll-lock');
-  } else {
-    mainNav.classList.add('header__nav--opened');
-    mainNav.classList.remove('header__nav--closed');
-    document.body.classList.add('scroll-lock');
-  }
-});
+if (mainNav && burgerButton) {
+  burgerButton.addEventListener('click', function () {
+    if (mainNav.classList.contains('header__nav--opened')) {
+      mainNav.classList.remove('header__nav--opened');
+      mainNav.classList.add('header__nav--closed');
+      document.body.classList.remove('scroll-lock');
+    } else {
+      mainNav.classList.add('header__nav--opened');
+      mainNav.classList.remove('header__nav--closed');
+      document.body.classList.add('scroll-lock');
+    }
+  });
+}
+
 
 if (window.innerWidth <= 767 && mainNav && burgerButton && burgerLink) {
   mainNav.classList.remove('header__nav--opened');
@@ -26,10 +29,12 @@ if (window.innerWidth <= 767 && mainNav && burgerButton && burgerLink) {
   });
 }
 
-document.addEventListener('click', (e) => {
-  if (!mainNav.contains(e.target) && mainNav && burgerButton && window.innerWidth <= 767) {
-    mainNav.classList.remove('header__nav--opened');
-    mainNav.classList.add('header__nav--closed');
-    document.body.classList.remove('scroll-lock');
-  }
-});
+if (mainNav && burgerButton && window.innerWidth <= 767) {
+  document.addEventListener('click', (e) => {
+    if (!mainNav.contains(e.target)) {
+      mainNav.classList.remove('header__nav--opened');
+      mainNav.classList.add('header__nav--closed');
+      document.body.classList.remove('scroll-lock');
+    }
+  });
+}
