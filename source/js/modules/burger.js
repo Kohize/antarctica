9a0c2d6cfe96054ebf90cb97360a929b71d2b9eb
Artem <kohi881@gmail.com>
2023-04-14 12:58:07 +0300

    added overlay for burger menu

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -1,23 +1,26 @@
 const mainNav = document.querySelector('.header__nav');
 const burgerButton = document.querySelector('.header__burger');
 const burgerLink = document.querySelectorAll('.header__link');
+const backgroundOverlay = document.querySelector('.overlay');
 
-if (mainNav && burgerButton) {
+if (mainNav && burgerButton && backgroundOverlay) {
   burgerButton.addEventListener('click', function () {
     if (mainNav.classList.contains('header__nav--opened')) {
       mainNav.classList.remove('header__nav--opened');
       mainNav.classList.add('header__nav--closed');
       document.body.classList.remove('scroll-lock');
+      backgroundOverlay.classList.remove('overlay-active');
     } else {
       mainNav.classList.add('header__nav--opened');
       mainNav.classList.remove('header__nav--closed');
       document.body.classList.add('scroll-lock');
+      backgroundOverlay.classList.add('overlay-active');
     }
   });
 }
 
 
-if (window.innerWidth <= 767 && mainNav && burgerButton && burgerLink) {
+if (window.innerWidth <= 767 && mainNav && burgerButton && burgerLink && backgroundOverlay) {
   mainNav.classList.remove('header__nav--opened');
   mainNav.classList.add('header__nav--closed');
   burgerLink.forEach((element) => {
@@ -25,16 +28,18 @@ if (window.innerWidth <= 767 && mainNav && burgerButton && burgerLink) {
       mainNav.classList.remove('header__nav--opened');
       mainNav.classList.add('header__nav--closed');
       document.body.classList.remove('scroll-lock');
+      backgroundOverlay.classList.remove('overlay-active');
     });
   });
 }
 
-if (mainNav && burgerButton && window.innerWidth <= 767) {
+if (mainNav && burgerButton && window.innerWidth <= 767 && backgroundOverlay) {
   document.addEventListener('click', (e) => {
     if (!mainNav.contains(e.target)) {
       mainNav.classList.remove('header__nav--opened');
       mainNav.classList.add('header__nav--closed');
       document.body.classList.remove('scroll-lock');
+      backgroundOverlay.classList.remove('overlay-active');
     }
   });
 }
